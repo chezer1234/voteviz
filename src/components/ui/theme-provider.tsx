472bac3c416bafe8ad6785b7defe9c0b9128a08f
Lewis Marshall <lewis.marshall@appvia.io>
2025-04-19 16:04:40 +0100

    fix the hydration things

--- src/components/ui/theme-provider.tsx
+++ src/components/ui/theme-provider.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemesProviderProps } from "next-themes"
 
-interface ThemeProviderProps extends React.HTMLAttributes<HTMLDivElement> {
+interface ThemeProviderProps extends NextThemesProviderProps {
   children: React.ReactNode
 }
 
